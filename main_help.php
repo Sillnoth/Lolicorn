602ad4398eaaab2a186397cc7d34aabd5736cc86
Corentin <corentin.fouyaya@hotmail.fr>
2015-12-04 01:40:56 +0100

    rajout de la nav en php
    evite la duplication futile

---
+++ main_help.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Nom de l'application</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/css.css" rel="stylesheet">
+    <script src="bootstrap/js/jquery-2.1.4.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<?php
+include './inc/basic_print.inc';
+
+print_nav();
+?>
+
+</body>
+</html>
